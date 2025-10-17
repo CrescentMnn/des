c9a4b67f55c635c4319b078488696fe2d8bb930e
sonar <sonar@2806-10a6-0013-b358-2e3b-70ff-fe0a-2417.ipv6.infinitum.net.mx>
2025-10-17 13:08:53 -0600

    UPdated pc.h and added according pc.cpp

--- include/pc.h
+++ include/pc.h
@@ -3,14 +3,6 @@
 
 #include <bitset>
 
-const int pc1_table = {
-
-};
-
-const int pc2_table = {
-
-};
-
 std::bitset<56> pc1_function(const std::bitset<64>&);
 
 std::bitset<48> pc2_function(const std::bitset<56>&);
---
+++ src/pc.cpp
@@ -0,0 +1,10 @@
+#include "pc.h"
+
+#include <iostream>
+#include <bitset>
+
+const int pc1_table[]
+
+std::bitset<56> pc1_function(const std::bitset<64>& key){
+
+}
