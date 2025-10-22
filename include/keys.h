2dda82900d8f3b38873d6bea8e765b2553b63250
Moon <coeus.ti@outlook.com>
2025-10-22 15:00:55 -0600

    Deleted extra function | not used

--- include/keys.h
+++ include/keys.h
@@ -4,8 +4,6 @@
 #include <bitset>
 #include <array>
 
-std::array<std::bitset<28>, 2> split_function(const std::bitset<56>&);
-
 std::bitset<56> join_function(const std::array<std::bitset<28>, 2>&);
 
 std::bitset<28> shift_function(const std::bitset<28>&);
