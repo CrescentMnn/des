d5a51e6a93e079ae0397b4b3a5953e6542081e60
Moon <coeus.ti@outlook.com>
2025-10-22 17:06:26 -0600

    Created basic shell for encryption rounds | missing S-Boxes imp

---
+++ src/encryption.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+/* User Headers */
+
+#include "ip.h"
+
+const int expansion_table[64] = {
+	32, 1, 2, 3, 4, 5,
+	4, 5, 6, 7, 8, 9,
+	8, 9, 10, 11, 12, 13,
+	12, 13, 14, 15, 16, 17,
+	16, 17, 18, 19, 20, 21,
+	20, 21, 22, 23, 24, 25,
+	24, 25, 26, 27, 28, 29,
+	28, 29, 30, 31, 32, 1
+};
+
+const int permutation_table[32] = {
+	16, 7, 20, 21, 29, 12, 28, 17,
+	1, 15, 23, 26, 5, 18, 31, 10,
+	2, 8, 24, 14, 32, 27, 3, 9,
+	19, 13, 30, 6, 22, 11, 4, 25
+};
+
+
+
+std::bitset<32> feistel_function(const std::bitset<32>& right_half, const std::bitset<48>& round_key){
+    
+}
+
+std::bitset<64> encryption_round(const std::bitset<64>& plaintext, const std::array<std::bitset<48>, 16>& round_keys){
+    
+}
